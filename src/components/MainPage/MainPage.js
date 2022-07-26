6d52d7b5b4bc19d4e03e6989aabd6f4169ad3518
Roman Shevts <Shevts.Roman@gmail.com>
2022-07-26 20:42:57 +0300

    added  descriprion and play button to random movie

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -12,7 +12,7 @@ export default function MainPage() {
   const [categories, setCategories] = useState([]);
 
   let random = Math.floor(Math.random() * popularMovies.length);
-
+console.log(popularMovies)
   useEffect(() => {
     getVideos("movie", setPopularMovies);
     getGenres("movie", setCategories);
@@ -28,9 +28,26 @@ export default function MainPage() {
               : null
           }
         ></img>
-        <div className="image_overlay"></div>
+        <div className="image__overlay"></div>
       </div>
       <div className="movies__container">
+        <div className="random_movie">
+          <div className="image__overlay_about-film">
+            <div className="about-film__title">
+              {popularMovies.length
+                ? popularMovies[random].title || popularMovies[random].name
+                : null}
+            </div>
+            <div className="about-film__overview">
+              {popularMovies.length ? popularMovies[random].overview : null}
+            </div>
+          </div>
+          <div className="play-button">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+              <path d="M361 215C375.3 223.8 384 239.3 384 256C384 272.7 375.3 288.2 361 296.1L73.03 472.1C58.21 482 39.66 482.4 24.52 473.9C9.377 465.4 0 449.4 0 432V80C0 62.64 9.377 46.63 24.52 38.13C39.66 29.64 58.21 29.99 73.03 39.04L361 215z" />
+            </svg>
+          </div>
+        </div>
         <Swiper
           modules={[Navigation]}
           navigation
@@ -41,18 +58,22 @@ export default function MainPage() {
             if (!categories.some((category) => category.id === item.id)) return;
             return (
               <SwiperSlide>
-                <div className="cont__player">
+                <div className="category__player">
                   <video loop muted src={item.src}></video>
                   <div
                     className="category"
                     onMouseOver={(e) => {
-                      e.target.closest(".cont__player").childNodes[0].play();
+                      e.target
+                        .closest(".category__player")
+                        .childNodes[0].play();
                     }}
                     onMouseOut={(e) => {
                       e.target.closest(
-                        ".cont__player"
+                        ".category__player"
                       ).childNodes[0].currentTime = 0;
-                      e.target.closest(".cont__player").childNodes[0].pause();
+                      e.target
+                        .closest(".category__player")
+                        .childNodes[0].pause();
                     }}
                   >
                     {item.name}
