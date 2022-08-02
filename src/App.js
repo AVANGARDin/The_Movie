a1d2431fad829abc8b3fb3f54f784a28204b3c33
Roman Shevts <Shevts.Roman@gmail.com>
2022-08-02 12:24:09 +0300

    added GenresPage Component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import MainPage from "./components/MainPage/MainPage";
 import MoviePage from "./components/MoviePage/MoviePage";
+import GenresPage from "./components/shared/GenresPage/GenresPage";
 
 
 function App() {
   return (
-    <Routes>
+    // <Routes>
+    //   <Route path="/" element={<Header />}>
+    //     <Route index element={<MainPage />} />
+    //     <Route path="movie/:name" element={<MoviePage />} />
+    //     {/* <Route path="tv/:name" element={<MoviePage />} /> */}
+    //   </Route>
+    // </Routes>
+
+        <Routes>
       <Route path="/" element={<Header />}>
-        <Route index element={<MainPage />} />
-        <Route path="movie/:name" element={<MoviePage />} />
+        <Route index element={<GenresPage />} />
+        {/* <Route path="movie/:name" element={<MoviePage />} /> */}
         {/* <Route path="tv/:name" element={<MoviePage />} /> */}
       </Route>
     </Routes>
+
   );
 }
 
